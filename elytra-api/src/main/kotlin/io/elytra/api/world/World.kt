5b0b784282326bdc0872faadc5ddbeb9684ab6d0
Wiljafor1 <willian.programadorce@gmail.com>
2020-03-27 22:41:57 -0300

    Start world object

--- elytra-api/src/main/kotlin/io/elytra/api/world/World.kt
+++ elytra-api/src/main/kotlin/io/elytra/api/world/World.kt
@@ -1,4 +1,11 @@
 package io.elytra.api.world
 
+import java.util.*
+
 interface World {
+	var uuid: UUID//Maybe remove this, i think we can use this to identify
+	var name: String
+	var difficulty: Difficulty
+	var type: WorldType
+	var mode: WorldMode
 }
---
+++ elytra-api/src/main/kotlin/io/elytra/api/world/WorldMode.kt
@@ -0,0 +1,6 @@
+package io.elytra.api.world
+
+enum class WorldMode(val compact: String) {
+	READ_WRITE("rw"),
+	READ_ONLY("ro")
+}
