6481910f35ebe232d5865f34d3040b894cea4756
Jailson Pereira <jailsondev70@gmail.com>
2020-03-30 10:38:48 -0300

    Add clickEvent to ChatComponent

--- elytra-api/src/main/kotlin/io/elytra/api/chat/ChatComponent.kt
+++ elytra-api/src/main/kotlin/io/elytra/api/chat/ChatComponent.kt
@@ -3,6 +3,7 @@ package io.elytra.api.chat
 
 data class ChatComponent(
 	val text: String,
+	val clickEvent: ClickEvent? = null,
 	val extra: MutableList<Extra>? = null
 ) {
 	data class Extra(val text: String, val formatOption: TextFormat)
--- elytra-sdk/src/main/kotlin/io/elytra/sdk/network/protocol/handlers/play/ChatHandler.kt
+++ elytra-sdk/src/main/kotlin/io/elytra/sdk/network/protocol/handlers/play/ChatHandler.kt
@@ -1,14 +1,17 @@
 package io.elytra.sdk.network.protocol.handlers.play
 
 import com.flowpowered.network.MessageHandler
+import io.elytra.api.chat.Action
 import io.elytra.api.chat.ChatComponent
 import io.elytra.api.chat.ChatMode
+import io.elytra.api.chat.ClickEvent
+import io.elytra.api.utils.asJson
 import io.elytra.sdk.network.NetworkSession
 import io.elytra.sdk.network.protocol.message.play.ChatMessage
 import io.elytra.sdk.network.protocol.message.play.OutboundChatMessage
 
 class ChatHandler : MessageHandler<NetworkSession, ChatMessage> {
 	override fun handle(session: NetworkSession, message: ChatMessage) {
-		session.send(OutboundChatMessage(ChatComponent(message.content), ChatMode.PLAYER))
+		session.send(OutboundChatMessage(message.content, ChatMode.PLAYER))
 	}
 }
