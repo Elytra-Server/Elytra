d161bf5c81a03d0116ec1a6bd425fcb35e897bd4
Bruno Martins <bruno.martins1998@hotmail.com>
2020-04-05 22:42:08 +0100

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,18 +35,14 @@ allprojects {
     dependencies {
         // Kotlin stuff
         implementation(kotlin("stdlib-jdk8"))
-        implementation(kotlin("reflect"))
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
         // Project dependencies
         implementation("com.google.code.gson:gson:2.8.6")
         implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
         implementation("io.projectreactor.netty:reactor-netty:0.9.6.RELEASE")
         implementation("com.flowpowered:flow-network:1.0.0")
         implementation("com.mojang:authlib:1.5.21")
-        implementation("org.apache.commons:commons-lang3:3.9")
         // Logging dependencies
         implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
-        implementation("ch.qos.logback:logback-core:1.2.3")
         implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
 
         // Dependencies required for unit tests
