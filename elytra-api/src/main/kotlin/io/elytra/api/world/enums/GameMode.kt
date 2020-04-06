1e35cda9d21cd342af60698d85fb271841ad35fd
chico ferreira <franciscomferreira2311@gmail.com>
2020-04-06 19:51:53 +0100

    fix: spectator gamemode feat: gamemodes can now be gotten by its id, name and some aliases

--- elytra-api/src/main/kotlin/io/elytra/api/world/enums/GameMode.kt
+++ elytra-api/src/main/kotlin/io/elytra/api/world/enums/GameMode.kt
@@ -1,8 +1,41 @@
 package io.elytra.api.world.enums
 
-enum class GameMode(val value: Int) {
-    SURVIVAL(0),
-    CREATIVE(1),
-    ADVENTURE(2),
-    HARDCORE(3)
+enum class GameMode(val value: Int, val aliases: List<String>) {
+
+    SURVIVAL(0, listOf("s")),
+    CREATIVE(1, listOf("c")),
+    ADVENTURE(2, listOf("a")),
+    SPECTATOR(3, listOf("spec"));
+
+    companion object {
+
+        fun get(string: String): GameMode? {
+            var gamemode: GameMode? = getByName(string)
+
+            if (gamemode == null) {
+                val int: Int? = string.toIntOrNull()
+                if (int != null) {
+                    gamemode = getByIdValue(int)
+                }
+            }
+
+            if (gamemode == null) {
+                gamemode = getByAlias(string)
+            }
+
+            return gamemode
+        }
+
+        fun getByName(name: String): GameMode? = values().firstOrNull { gameMode ->
+            gameMode.name.equals(name, ignoreCase = true)
+        }
+
+        fun getByIdValue(value: Int): GameMode? = values().firstOrNull { gameMode ->
+            gameMode.value == value
+        }
+
+        fun getByAlias(alias: String): GameMode? = values().firstOrNull { gameMode ->
+            gameMode.aliases.any { s -> s.equals(alias, ignoreCase = true) }
+        }
+    }
 }
--- elytra-sdk/src/main/kotlin/io/elytra/sdk/commands/GamemodeCommand.kt
+++ elytra-sdk/src/main/kotlin/io/elytra/sdk/commands/GamemodeCommand.kt
@@ -7,7 +7,6 @@ import io.elytra.api.command.argument.ArgumentList
 import io.elytra.api.command.argument.ArgumentTypes
 import io.elytra.api.entity.Player
 import io.elytra.api.world.enums.GameMode
-import io.elytra.sdk.command.handler.ElytraCommandHandler
 import io.elytra.sdk.utils.localeMessage
 
 @CommandInfo(label = "gamemode")
@@ -21,18 +20,17 @@ class GamemodeCommand : ElytraCommand() {
         val player = sender as Player
         val arg = arguments.getValue<String>(0)
 
-        try {
-            val gamemode = GameMode.valueOf(arg.toUpperCase())
+        val gamemode = GameMode.get(arg)
 
-            player.gamemode = gamemode
+        if (gamemode == null) {
+            player.localeMessage("command.gamemode.failure").with("gamemode", arg)
+            return
+        }
 
-            player.localeMessage("command.gamemode.success") {
-                with("gamemode", gamemode.name.toLowerCase())
-            }
-        } catch (e: ElytraCommandHandler.CommandException) {
-            player.localeMessage("command.gamemode.failure") {
-                with("gamemode", arg)
-            }
+        player.gamemode = gamemode
+
+        player.localeMessage("command.gamemode.success") {
+            with("gamemode", gamemode.name.toLowerCase())
         }
     }
 }
