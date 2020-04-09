fa34ce1fd6acaab56d6790b7a6919ea94d0f0d58
Tomás Duarte <tomasrsduarte@gmail.com>
2020-04-09 17:01:16 +0100

    change: target and retention on commandspec argument

--- elytra-api/src/main/kotlin/io/elytra/api/command/CommandSpec.kt
+++ elytra-api/src/main/kotlin/io/elytra/api/command/CommandSpec.kt
@@ -6,6 +6,8 @@ package io.elytra.api.command
  * @param label the label after the command prefix, ex: /<label here>
  * @param usage the usage message of the command
  */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class CommandSpec(
     val label: String,
     val usage: String = ""
