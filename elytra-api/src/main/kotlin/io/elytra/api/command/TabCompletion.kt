5ecff760ea8711c733ba69baf1adf1a7a219874e
Bruno Martins <bruno.martins1998@hotmail.com>
2020-05-01 16:16:45 +0100

    Fix logger name inside the log files

--- elytra-api/src/main/kotlin/io/elytra/api/command/TabCompletion.kt
+++ elytra-api/src/main/kotlin/io/elytra/api/command/TabCompletion.kt
@@ -4,9 +4,9 @@ import io.elytra.api.chat.TextComponent
 
 data class TabCompletion(
     /** The index where the completion begins. Default it's the beginning of the last word */
-    var startIndex: Int = 0,
+    var startIndex: Int,
     /** The length of the text to replace after the [startIndex]. Default it's the whole word. */
-    var textLength: Int = 0,
+    var textLength: Int,
     /** List of completions to show to the client */
     var completions: MutableList<Completion> = mutableListOf()
 ) {
@@ -17,7 +17,6 @@ data class TabCompletion(
         /** The tooltip to show when hovering the completion */
         var tooltip: TextComponent?
     ) {
-        constructor(match: String) : this(match, match)
-        constructor(match: String, tooltip: String) : this(match, TextComponent(tooltip))
+        constructor(match: String, tooltip: String? = null) : this(match, tooltip?.let { TextComponent(it) })
     }
 }
